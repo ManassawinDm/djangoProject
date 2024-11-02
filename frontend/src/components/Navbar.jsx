9290b3f7ab4382de2c9c05d6482cd3b3c7c4500c
pstgame1159 <s6404062610375@email.kmutnb.ac.th>
2024-11-02 13:47:41 +0700

    delete log user
    delete

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -60,7 +60,6 @@ function Navbar() {
     try {
       const res = await api.post("/userinfo/", { user_id: decode.user_id });
       const data = res.data;
-      console.log(data.permission_user)
       if (data.permission_user) {
         localStorage.setItem('permission_user', data?.permission_user);
         navigate('/home');
